47da08e81fac0e8b3f61813054a94ea0f7752af5
Fabiano S. Ramos <fabiano.suporteinfo@gmail.com>
2022-02-05 21:56:15 -0300

    Agents created

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,5 +1,14 @@
 defmodule Exlivery do
-  def hello do
-    :world
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
+  alias Exlivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrders
+
+  def start_agents do
+    UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrders, as: :call
 end
